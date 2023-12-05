dfa45cd648cace25b825b20ccd21ba8dbfe3ee2c
AnthonyDWeb <delforge.anthony@gmail.com>
2023-12-05 09:21:01 +0100

    try to resolve url error loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import { Homepage } from "./pages";
 import Gameraid from "./pages/gameraid";
 
 function App() {
+  const path = window.location.pathname;
+  console.log("path",path);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="arkhanya.privacy" element={<Homepage />} />
-        <Route path="arkhanya.privacy/gameraid" element={<Gameraid />} />
+        <Route path="/arkhanya.privacy" element={<Homepage />} />
+        <Route path="/arkhanya.privacy/gameraid" element={<Gameraid />} />
       </Routes>
     </BrowserRouter>
   );
